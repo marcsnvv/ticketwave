1bf8aad780fc8914af469f97df80381135c52abb
ImBusto <vuntagecom@gmail.com>
2024-11-15 16:40:36 +0100

    improved calendar componente

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -1,10 +1,11 @@
-"use client";
+"use client"
+
 import * as React from "react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 
 function Calendar({
   className,
@@ -13,7 +14,7 @@ function Calendar({
   ...props
 }) {
   return (
-    (<DayPicker
+    <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
@@ -57,11 +58,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      {...props} />)
-  );
+      {...props}
+    />
+  )
 }
 Calendar.displayName = "Calendar"
 
